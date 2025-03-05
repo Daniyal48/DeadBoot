db589680f037ffe787be024805c1e59f20e85ed8
Daniyal Shahzad <28977993+Daniyal48@users.noreply.github.com>
2025-03-05 13:11:16 +0500

    Update build.sh
    Added Support for 64-Bit but changes will take affect in the next few updates when we shift to Kernel and OS development. Currently I am only focusing to make the bootloader better.

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 export PREFIX="$HOME/opt/cross"
-export TARGET=i686-elf
+export TARGET32=i686-elf
+#export TARGET64=x86_64-elf
 export PATH="$PREFIX/bin:$PATH"
+
+make clean
 make all
